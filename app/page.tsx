dd443008d3fc107b1742567a39652bd0b5115aaa
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-02-20 22:22:05 +0000

    chore: update index page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 export default function Home() {
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Clerk starter</h1>
+    <>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 flex flex-col items-center justify-center rounded-xl p-4">
+          <p>Total balance</p>
+          <p>-€43058.38</p>
+        </div>
+        <div className="bg-muted/50 flex flex-col items-center justify-center rounded-xl p-4">
+          <p>You owe</p>
+          <p>€43081.05</p>
+        </div>
+        <div className="bg-muted/50 flex flex-col items-center justify-center rounded-xl">
+          <p>You are owed</p>
+          <p>€22.67</p>
+        </div>
       </div>
-    </section>
+      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci
+        dolore molestiae similique totam? Aspernatur dolor doloribus hic itaque,
+        mollitia nobis, perferendis, praesentium recusandae sint suscipit
+        tempora tenetur ullam veritatis.
+      </div>
+    </>
   )
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -27,6 +27,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem
 } from '@/components/ui/sidebar'
+import Link from 'next/link'
 
 const data = {
   navMain: [
@@ -154,15 +155,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
+              <Link href="/">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Acme Inc</span>
+                  <span className="truncate font-medium">Splitwiser</span>
                   <span className="truncate text-xs">Enterprise</span>
                 </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
